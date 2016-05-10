11c7daafdde2119b7de7a10eb591e679b2a78638
Arkotek <misarmlimited@gmail.com>
2016-05-10 14:30:37 +0000

    fix tests

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,6 @@ export NODE_ENV test
 $mocha test/task_manager.mocha.js
 $mocha test/compress.mocha.js
 $mocha test/file_manager.mocha.js
-$mocha test/forum.mocha.js
 $mocha test/socket_router.mocha.js
 $mocha test/network.mocha.js
 $mocha test/multi_tasks.mocha.js
