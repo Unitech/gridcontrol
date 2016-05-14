6d3d8854f6eca8db8c7dc305bca637cfe1c8022e
Unitech <strzelewicz.alexandre@gmail.com>
2016-05-14 17:23:41 +0200

    move scaleway-commander to another repo

--- subprojects/scaleway/toto.sh
+++
@@ -1,2 +0,0 @@
-
-bash -c "npm install -g json; RET=`node -e "var pkg = require('./package.json'); console.log(Object.keys(pkg.dependencies))"`; json -I -f package.json -e \"this.bundledDependencies=$RET\""
