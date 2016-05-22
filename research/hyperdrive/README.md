68a66c0407ce887eb1b999bac30d9731e3243aeb
Unitech <strzelewicz.alexandre@gmail.com>
2016-05-22 21:48:20 +0200

    drop signalhub, create a COMMAND grid(swarm)

---
+++ research/hyperdrive/README.md
@@ -0,0 +1,14 @@
+
+# Hyperdrive
+
+```bash
+# Start a "file slave"
+$ node one.js
+# Start a second "file slave"
+$ node one.js
+# Start the "file master"
+$ node two.js
+```
+
+- [ ] Pas de tarball (on transfere que les fichiers modifiés?)
+- [ ] 2 modules compilés (rabit + sodium-prebuilt)
--- research/hyperdrive/two.js
+++ research/hyperdrive/two.js
@@ -1,78 +1,82 @@
+'use strict';
+
 const signalhub = require('signalhub')
-var Swarm = require('discovery-swarm')
-var Hyperdrive = require('hyperdrive')
-var Level = require('memdb')
-var raf = require('random-access-file')
-const bluebird = require('bluebird')
-const fs = bluebird.promisifyAll(require('fs'))
-const p = require('path')
+var Swarm       = require('discovery-swarm')
+var Hyperdrive  = require('hyperdrive')
+var Level       = require('memdb')
+var raf         = require('random-access-file')
+const bluebird  = require('bluebird')
+const fs        = bluebird.promisifyAll(require('fs'))
+const p         = require('path')
 
-const Archiver = require('./archiver.js')
+const Archiver  = require('./archiver.js')
 
-const CURRENT_ROOT = __dirname
-
-// here are the default config dat uses:
-// used for MDNS and also as the dns 'app name', you prob shouldnt change this
-var DAT_DOMAIN = 'dat.local'
-// dat will try this first and pick the first open port if its taken
-var DEFAULT_LOCAL_PORT = 3282 
-// we run the servers below you can use if you want or run your own
-var DEFAULT_DISCOVERY = [
+const CURRENT_ROOT     = __dirname
+var DAT_DOMAIN         = 'dat.local'
+var DEFAULT_LOCAL_PORT = 3282
+var DEFAULT_DISCOVERY  = [
   'discovery1.publicbits.org',
   'discovery2.publicbits.org'
 ]
 
+let current_link = null;
+
 var db = Level('./dat2.db')
 var drive = Hyperdrive(db)
 
-const hub = signalhub('test', ['http://localhost:8080'])
-
+// Grid about commands (MPI)
 var swarm = Swarm({
-  id: drive.core.id,
-  dns: {server: DEFAULT_DISCOVERY, domain: DAT_DOMAIN, interval: 1000},
-  dht: false,
+  dns: {
+    server: DEFAULT_DISCOVERY,
+    domain: DAT_DOMAIN,
+    interval: 1000
+  },
+  dht: false
 })
+
+swarm.join('COMMAND-LINK')
+
+// Grid about files sharing (P2P)
+var fileSwarm = Swarm({
+  dns: {
+    server: DEFAULT_DISCOVERY,
+    domain: DAT_DOMAIN,
+    interval: 1000
+  },
+  dht: false
+})
+
+swarm.on('connection', function(peer) {
+  // If neew peer is connected, ask peer to SYNC
+  if (current_link) {
+    peer.write(JSON.stringify({
+      cmd : 'sync',
+      link : current_link
+    }));
+  }
+});
 
 swarm.listen()
 
 let archiver = new Archiver({
-  root: CURRENT_ROOT,
-  drive: drive,
-  interplanetary: swarm
+  root           : CURRENT_ROOT,
+  drive          : drive,
+  interplanetary : fileSwarm
 })
 
 archiver.archive('./sharethis')
-.then(archive => {
-  return archiver.spread(archive)
-})
-.then(link => {
-  hub.broadcast('sync', link, function() {
-    console.log('broadcasted %s', link)
+  .then(archive => {
+    return archiver.spread(archive)
   })
-})
+  .then(link => {
+    current_link = link;
 
-setTimeout(function() {
-  swarm.destroy(function() {
-
-    var swarm = Swarm({
-      id: drive.core.id,
-      dns: {server: DEFAULT_DISCOVERY, domain: DAT_DOMAIN, interval: 1000},
-      dht: false,
-    })
-
-    swarm.listen()
-
-    archiver.interplanetary = swarm
-
-    archiver.archive('./sharethat')
-    .then(archive => {
-      return archiver.spread(archive)
-    })
-    .then(link => {
-      hub.broadcast('sync', link, function() {
-        console.log('broadcasted %s', link)
-      })
-    })
+    // Broadast to all peers already connected
+    Object.keys(swarm._peersIds).forEach(function(key) {
+      console.log('Sending to', key);
+      swarm._peersIds[key].write(JSON.stringify({
+        cmd : 'sync',
+        link : current_link
+      }));
+    });
   })
-
-}, 2000)
