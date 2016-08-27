8cad42e663e5a215bb3575ea60d6b5d5d1d87e5b
Unitech <strzelewicz.alexandre@gmail.com>
2016-08-27 09:17:12 +0200

    README.md + grid detect command

--- sample-proj/index.js
+++ sample-proj/index.js
@@ -1,13 +1,12 @@
 
 var grid = require('../grid-api/').init({
   task_folder : 'tasks',
-  instances   : 2,
+  instances   : 1,
+  local : false,
   env         : {
     NODE_ENV : 'development'
   }
 });
-
-
 
 grid.on('ready', function() {
   console.log('Ready');
@@ -15,8 +14,8 @@ grid.on('ready', function() {
   setInterval(function() {
 	  grid.dispatch('request-test', {
 	    url : 'https://api.ipify.org/?format=json'
-	  }, function(err, data) {
-      if (err) console.log(err);
+	  }, function(err, data, server) {
+      if (err) console.log(err, server.public_ip);
 	    console.log('Got data', data);
 	  });
   },1050);
@@ -25,11 +24,9 @@ grid.on('ready', function() {
     grid.exec('echo', {
       name : 'hey'
     }, function(err, data, server) {
+      if (err) console.log(err);
 	   // console.log(err, data, server && server.public_ip);
-	    //console.log('Got response');
+	    console.log('Got response', data, server.public_ip);
     });
   }, 1000);
-
-
-
 });
