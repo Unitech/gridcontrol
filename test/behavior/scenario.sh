e155019b1474ae5949c59f3f27b4665986c60f47
Unitech <strzelewicz.alexandre@gmail.com>
2016-09-04 17:57:49 +0800

    (test) add behavioral tests

--- test/behavior/scenario.sh
+++ test/behavior/scenario.sh
@@ -5,10 +5,9 @@ source "${SRC}/include.sh"
 export NODE_ENV='test'
 export GRID='test-grid-scenario'
 
-#npm install pm2@next -g
-#pm2 install gridcontrol
-
-#npm install grid-cli -g
+npm install pm2@next -g
+pm2 install gridcontrol
+npm install grid-cli -g
 
 #
 # Grid listing
